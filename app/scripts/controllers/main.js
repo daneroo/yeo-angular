8783023342f6ec173d5ae27eaaac32c6b4c67c30
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-25 14:22:41 -0400

    start adding my own data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,4 +6,18 @@ yeoAngularApp.controller('MainCtrl', function($scope) {
     'AngularJS',
     'Testacular'
   ];
+  $scope.gridWatch = {
+    stamp: ""+new Date(),
+    power:{value:16439, units:'MW', level:'AVG'},
+    emissions:{value:1068, units:'tonnes'},
+    powerByType:[
+    {kind:'Nuclear', power:10247},
+    {kind:'Hydro', power:4044},
+    {kind:'Gas', power:1596},
+    {kind:'Coal', power:553},
+    {kind:'Wind', power:550},
+    {kind:'Other', power:167}
+    ]
+  };
+  
 });
